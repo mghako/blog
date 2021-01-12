393bb63817b9796c1bd6e8fe6d359d91ca0964a5
mghako <htetaungko@yahoo.com>
2021-01-12 22:22:39 +0630

    added navbar in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,15 +10,24 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&family=Raleway&family=Ubuntu&display=swap" rel="stylesheet">
         <!-- Bootstrap css cdn -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <!-- custom css -->
         <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     </head>
     <body id="app">
         
         <h1 class="text-center">Welcome to Blog</h1>
+        
         <div class="container">
             <div class="row">
+                <div class="col-md-12 mb-4">
+                    {{-- include navbar for frontend  --}}
+                    @include('partials.navbar')
+                </div>
+            </div>
+            <h4>Latest Posts</h4>
+            <div class="row">
+           
             @foreach($posts as $post)
                 <div class="col-md-4">
                     <div class="card">
@@ -31,7 +40,9 @@
             @endforeach
             </div>
         </div>
+        
         <!-- bootstrap js cdn -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.min.js" integrity="sha512-8qmis31OQi6hIRgvkht0s6mCOittjMa9GMqtK9hes5iEQBQE/Ca6yGE5FsW36vyipGoWQswBj/QBm2JR086Rkw==" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     </body>
 </html>
